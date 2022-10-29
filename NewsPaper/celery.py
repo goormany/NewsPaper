a4dc49c20d2927114df889e0b553df0b6b3fa4f9
goormany <zarbot351@yandex.ru>
2022-10-29 14:08:15 +0300

    add weekly newsletter for celery

--- NewsPaper/celery.py
+++ NewsPaper/celery.py
@@ -1,5 +1,6 @@
 import os
 from celery import Celery
+from celery.schedules import crontab
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'NewsPaper.settings')
 
@@ -7,3 +8,11 @@ app = Celery('NewsPaper')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
 app.autodiscover_tasks()
+
+
+app.conf.beat_schedule = {
+    'notify_email_weeks': {
+        'task': 'news.tasks.notify_email_weeks',
+        'schedule': crontab(minute='0', hour='8', day_of_week='sun')
+    },
+}
