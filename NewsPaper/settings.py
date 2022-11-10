6aaa732954f7c6a9085ce8c4a158993d5c9ea4b0
goormany <zarbot351@yandex.ru>
2022-11-10 17:51:10 +0300

    add secret key django to env

--- NewsPaper/settings.py
+++ NewsPaper/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-t!@_azvnrd#y-zdf5g&ec)rjmj66hi^24_cexm63s4o7edu-y4'
+SECRET_KEY = os.getenv("SECRET_KEY_DJANGO")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
